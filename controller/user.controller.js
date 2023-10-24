8d534ccf7911466f48911852b63e81ecaeda9e1b
MyWahyuSap <wahyusaputra222000@gmail.com>
2023-10-24 22:19:18 +0700

    i prisma

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,11 +1,22 @@
 const { ResponseTemplate } = require('../helper/template.helper')
 
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 function TestUser(req, res) {
     let resp = ResponseTemplate(null, 'Berhasil akses User', null, 200)
     res.json(resp)
 }
 
-function TestUserPost(req, res) {
+async function TestUserPost(req, res) {
+
+    const { name } = req.body
+
+    const payload = {
+        name,
+    }
+
     console.log(req.query.name)
     let objRest = {
         name: req.body.name,
